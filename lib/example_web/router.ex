4b657182d2ccde7450449855e586bb752f151838
Andrew <andrewianstewart@gmail.com>
2023-12-15 16:32:58 -0500

    Initial commit

---
+++ lib/example_web/router.ex
@@ -0,0 +1,37 @@
+defmodule ExampleWeb.Router do
+  use ExampleWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {ExampleWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", ExampleWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", ExampleWeb do
+  #   pipe_through :api
+  # end
+
+  # Enable Swoosh mailbox preview in development
+  if Application.compile_env(:example, :dev_routes) do
+
+    scope "/dev" do
+      pipe_through :browser
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
